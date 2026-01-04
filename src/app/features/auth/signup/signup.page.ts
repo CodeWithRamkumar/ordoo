07cd2bca57b3aeb1aa9816c7793bf06e9baae550
CodeWithRamkumar <ramkumar03251999@gmail.com>
2026-01-04 13:31:33 +0530

    Initial commit

--- src/app/features/auth/signup/signup.page.ts
+++ src/app/features/auth/signup/signup.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl } from '@angular/forms';
-import { IonContent, IonButton, IonInput, IonItem, IonIcon, NavController, IonText, ToastController } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonInput, IonItem, IonIcon, NavController, IonText, ToastController, IonLoading } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
 import { addIcons } from 'ionicons';
 import { eyeOutline, eyeOffOutline, mailOutline, lockClosedOutline, personOutline, logoGoogle, logoFacebook, logoApple, logoTwitter } from 'ionicons/icons';
@@ -15,7 +15,7 @@ import { FirebaseSsoService } from '../../../shared/services/firebase-sso.servic
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonContent, IonButton, IonInput, IonItem, IonIcon, IonText, CommonModule, ReactiveFormsModule]
+  imports: [IonContent, IonButton, IonInput, IonItem, IonIcon, IonText, IonLoading, CommonModule, ReactiveFormsModule]
 })
 export class SignupPage implements OnInit {
   signupForm: FormGroup;
@@ -80,14 +80,17 @@ export class SignupPage implements OnInit {
     }
 
     console.log('Making API call to:', this.config.AUTH.SIGNUP, 'with data:', signupData);
+    await this.loader.show('Creating account...');
     
     try {
       const response = await this.http.post(this.config.AUTH.SIGNUP, signupData).toPromise();
       console.log('Signup success:', response);
+      await this.loader.hide();
       // await this.authService.saveUserData(response);
       this.navCtrl.navigateRoot('/auth/profile-setup');
     } catch (error) {
       console.log('Signup error:', error);
+      await this.loader.hide();
     }
   }
 
